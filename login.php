792efb35ccc7ee20653b5be0463442a6f5223854
Arsema <arsemayemiru610@gmail.com>
2024-03-12 12:27:22 -0400

    login

--- login.php
+++ login.php
@@ -1,52 +1,21 @@
-<?php 
-session_start(); 
-include "db_conn.php";
+<!DOCTYPE html>
+<html>
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
-	}
+<body>
+	<form action="login-check.php" method="post" class="login">
+		<h2>LOGIN</h2>
+		<label>User Name</label>
+		<input type="text" name="username" placeholder="User Name"><br>
 
-	$username = validate($_POST['username']);
-	$password = validate($_POST['password']);
+		<label>Password</label>
+		<input type="password" name="password" placeholder="Password"><br>
 
-	if (empty($username)) {
-		header("Location: index.php?error=User Name is required");
-	    exit();
-	}else if(empty($password)){
-        header("Location: index.php?error=Password is required");
-	    exit();
-	}else{
-		$sql = "SELECT * FROM users WHERE user_name='$username' AND password='$password'";
+		<button type="submit">Login</button>
+	</form>
+</body>
 
-		$result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['user_name'] === $username && $row['password'] === $password) {
-            	$_SESSION['user_name'] = $row['user_name'];
-            	$_SESSION['name'] = $row['name'];
-            	$_SESSION['id'] = $row['id'];
-            	header("Location: home.php");
-		        exit();
-            }else{
-				// header("Location: index.php?error=Incorect User name or password");
-				echo "1 Incorect User name or password";
-		        // exit();
-			}
-		}else{
-			// header("Location: index.php?error=Incorect User name or password");
-			echo "2 Incorect User name or password";
-	        // exit();
-		}
-	}
-	
-}else{
-	// header("Location: index.php");
-	echo "3 Incorect User name or password";
-	exit();
-}
-
+</html>
