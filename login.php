7cc304e181a082bd265f9c6c4073ba5218c7fbe0
Arsema <arsemayemiru610@gmail.com>
2024-03-13 16:29:22 -0400

    style changes

--- login.php
+++ login.php
@@ -6,7 +6,8 @@
 </head>
 
 <body>
-	<form action="login-check.php" method="post" class="login">
+	
+	<form  action="login-check.php" method="post" class="login">
 		<h2>LOGIN</h2>
 		<label>User Name</label>
 		<input type="text" name="username" placeholder="User Name"><br>
