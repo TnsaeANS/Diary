aaae64b43c36695fdb1f4367cc05e7263fdbb7bc
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2024-03-17 14:06:25 +0300

    search function

--- home.php
+++ home.php
@@ -1,10 +1,12 @@
 <?php
+include 'db_conn.php';
 session_start();
 $theme = isset($_SESSION['theme']) ? $_SESSION['theme'] : 'light';
 
 if (isset($_GET['toggle_theme'])) {
     $theme = ($theme === 'light') ? 'dark' : 'light';
     $_SESSION['theme'] = $theme;
+    
 }
 if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 ?>
@@ -18,10 +20,16 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 <body>
     <div class="navbar">
         <div class="diary">Diary</div>
-        <div class="search-bar">
-            <input type="text" placeholder=" Search" />
-            <button>Search</button>
+        
+        <form method="post">
+            <div class="search-bar">
+            <input name="search" type="text" placeholder=" Search" />
+            <button name="submit">Search</button>
         </div>
+        </form>    
+
+        
+        
         <div class="actions">
             <p class="profile"><?php echo $_SESSION['name']; ?></p>
             <a class="logout" href="logout.php">Logout</a>
@@ -31,10 +39,41 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
      <div class="homeBody">
      <h1 class="home">Hello, <?php echo $_SESSION['name']; ?></h1>
-     <button class="profile-button" onclick="location.href='profile.php';">Profile</button>
+     <button  onclick="location.href='profile.php';">Profile</button>
      <button onclick="location.href='display.php';">Diary</button>
-
-
+     </div>
+<div>
+            <table>
+                <?php
+                if(isset($_POST['submit'])){
+                    $search=$_POST['search'];
+                    $user_id = $_SESSION['id'];
+                    $sql="SELECT * FROM `crud` WHERE id='$search' AND user_id='$user_id'";
+                    $result=mysqli_query($conn, $sql);
+                    if(mysqli_num_rows($result)>0){
+                        echo '<thead>
+                            <tr>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Content</th> 
+                            </tr>  
+                              </thead>';
+                              
+                            $row=mysqli_fetch_assoc($result);
+                            echo '<tbody>
+                            <tr>
+                            <td>'.$row['id'].'</td>
+                            <td>'.$row['title'].'</td>
+                            <td>'.$row['content'].'</td> 
+                            </tr>
+                            </tbody>';
+                    }else{
+                        echo "There is no data like that.";
+                    }
+                    }
+                ?>
+            </table>    
+        </div>
 </body>
 </html>
 <?php
